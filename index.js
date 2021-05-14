7a03f9f53bda3ea973ace784bab5c01dddf0eba7
Matthieu Vachon <matthieu.vachon@dfuse.io>
2021-05-13 22:23:45 -0400

    Improved example and showcase how to use fork_steps

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ const bstreamService = loadGrpcPackageDefinition("dfuse/bstream/v1/bstream.proto
 
 const blockMsg = bstreamProto.root.lookupType("dfuse.bstream.v1.Block")
 const eosioBlockMsg = eosioProto.root.lookupType("dfuse.eosio.codec.v1.Block")
+const forkStepEnum = bstreamProto.root.lookupEnum("dfuse.bstream.v1.ForkStep")
+
+const forkStepNew = forkStepEnum.values["STEP_NEW"]
+const forkStepUndo = forkStepEnum.values["STEP_UNDO"]
+const forkStepIrreversible = forkStepEnum.values["STEP_IRREVERSIBLE"]
 
 async function main() {
   if (process.argv.length <= 2) {
@@ -36,82 +41,97 @@ async function main() {
 
   const showFull = process.argv.length > 3 && process.argv[3] == "--full"
 
-  await new Promise(async (resolve, reject) => {
-    const metadata = new grpc.Metadata()
-    metadata.set("authorization", (await dfuse.getTokenInfo()).token)
+  try {
+    await new Promise(async (resolve, reject) => {
+      let stream
 
-    const stream = client.Blocks(
-      {
-        start_block_num: 150000000,
-        stop_block_num: 150000005,
-        include_filter_expr: 'action == "onblock"',
-      },
-      metadata
-    )
+      try {
+        const metadata = new grpc.Metadata()
+        metadata.set("authorization", (await dfuse.getTokenInfo()).token)
 
-    stream.on("data", (data) => {
-      const { block: rawBlock } = data
-      if (rawBlock.type_url !== "type.googleapis.com/dfuse.eosio.codec.v1.Block") {
-        rejectStream(stream, reject, invalidTypeError(rawBlock.type_url))
-        return
-      }
+        stream = client.Blocks(
+          {
+            start_block_num: 150000000,
+            stop_block_num: 150000005,
+            include_filter_expr: 'action == "onblock"',
+            // By default, step events received are `new`, `undo` and `irreversible`, for irreversible only, uncommented the following
+            // fork_steps: [forkStepIrreversible],
+          },
+          metadata
+        )
 
-      switch (data.step) {
-        case "STEP_NEW":
-          // Block is the new head block of the chain
-          break
-        case "STEP_UNDO":
-          // Block has been forked out, should undo everything
-          break
-        case "STEP_IRREVERSIBLE":
-          // Block is now irreversible, it's number will be ~360 blocks in the past
-          break
-      }
+        stream.on("data", (data) => {
+          const { block: rawBlock } = data
+          if (rawBlock.type_url !== "type.googleapis.com/dfuse.eosio.codec.v1.Block") {
+            rejectStream(stream, reject, invalidTypeError(rawBlock.type_url))
+            return
+          }
 
-      const block = eosioBlockMsg.decode(rawBlock.value)
+          switch (data.step) {
+            case "STEP_NEW":
+              // Block is the new head block of the chain
+              break
+            case "STEP_UNDO":
+              // Block has been forked out, should undo everything
+              break
+            case "STEP_IRREVERSIBLE":
+              // Block is now irreversible, it's number will be ~360 blocks in the past
+              break
+          }
 
-      // Use the "filtered" versions since all blocks returned by Firehose is always filtered
-      const transactionCount = block.filteredTransactionTraces.length
-      let actionCount = 0
-      let systemActionCount = 0
+          const step = data.step.toLowerCase().replace(/step_/, "")
+          const block = eosioBlockMsg.decode(rawBlock.value)
 
-      block.filteredTransactionTraces.forEach((trace) => {
-        trace.actionTraces.forEach((actionTrace) => {
-          actionCount += 1
+          // Use the "filtered" versions since all blocks returned by Firehose is always filtered
+          const transactionCount = block.filteredTransactionTraces.length
+          let actionCount = 0
+          let systemActionCount = 0
 
-          if (actionTrace.receiver === "eosio") {
-            systemActionCount += 1
+          block.filteredTransactionTraces.forEach((trace) => {
+            trace.actionTraces.forEach((actionTrace) => {
+              actionCount += 1
+
+              if (actionTrace.receiver === "eosio") {
+                systemActionCount += 1
+              }
+            })
+          })
+
+          console.log(
+            `Block #${block.number} (${block.id}, ${step}) - ${transactionCount} Transactions, ${actionCount} Actions (${systemActionCount} System Actions)`
+          )
+          if (showFull) {
+            console.log(JSON.stringify(block, null, "  "))
           }
         })
-      })
 
-      console.log(
-        `Block #${block.number} (${block.id}) - ${transactionCount} Transactions, ${actionCount} Actions (${systemActionCount} System Actions)`
-      )
-      if (showFull) {
-        console.log(JSON.stringify(block, null, "  "))
+        stream.on("error", (error) => {
+          rejectStream(stream, reject, error)
+        })
+
+        stream.on("status", (status) => {
+          if (status.code === 0) {
+            resolveStream(stream, resolve)
+            return
+          }
+
+          // On error, I've seen the "error" callback receiving it, so not sure in which case we would do something else here
+        })
+      } catch (error) {
+        if (stream) {
+          rejectStream(stream, reject, error)
+        } else {
+          reject(error)
+        }
       }
     })
-
-    stream.on("error", (error) => {
-      rejectStream(stream, reject, error)
-    })
-
-    stream.on("status", (status) => {
-      if (status.code === 0) {
-        resolveStream(stream, resolve)
-        return
-      }
-
-      // On error, I've seen the "error" callback receiving it, so not sure in which case we would do something else here
-    })
-  })
-
-  // Clean up resources, should be performed only if the gRPC client (`client` here) and/or the dfuse client
-  // (`dfuse` here) are not needed anymore. If you have pending stream, you should **not** close those since
-  // they are required to make the stream works correctly.
-  client.close()
-  dfuse.release()
+  } finally {
+    // Clean up resources, should be performed only if the gRPC client (`client` here) and/or the dfuse client
+    // (`dfuse` here) are not needed anymore. If you have pending stream, you should **not** close those since
+    // they are required to make the stream works correctly.
+    client.close()
+    dfuse.release()
+  }
 }
 
 function loadGrpcPackageDefinition(package) {
